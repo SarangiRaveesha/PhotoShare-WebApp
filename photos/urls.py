ee8450f7638079ea37cb568c4762a226d854b049
chamodyakaveen12 <chamodyakaveen12@gmail.com>
2026-04-24 11:40:07 +0530

    Upload Photo
    Added comment to the upload photo path for clarity.

--- photos/urls.py
+++ photos/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.feed, name='feed'),
+    #Upload Photo
     path('upload/', views.upload_photo, name='upload'),
     path('edit/<int:photo_id>/', views.edit_photo, name='edit_photo'),
     path('delete/<int:photo_id>/', views.delete_photo, name='delete_photo'),
