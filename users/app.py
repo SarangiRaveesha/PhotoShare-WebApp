42e2b670a8499f96ff927a1f34aede497775e638
rushani savishka <rushanisavishka@gmail.com>
2026-04-24 11:46:10 +0530

    Update comment format in UsersConfig

--- users/app.py
+++ users/app.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class UsersConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField' #(BigAutoField = a large auto-incrementing number)
+    default_auto_field = 'django.db.models.BigAutoField' #(BigAutoField -> a large auto-incrementing number)
     name = 'users'
