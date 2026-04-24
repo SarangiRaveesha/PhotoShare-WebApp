20fd71d203269cedd5eb7dac98dcff22c98dfcdb
rushani savishka <rushanisavishka@gmail.com>
2026-04-24 12:36:20 +0530

    Add comments to wsgi.py for clarity
    Added comments to improve code readability.

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,6 +1,10 @@
+# Import os module
 import os
+# creates the WSGI application
 from django.core.wsgi import get_wsgi_application
 
+# Set the default settings module for the Django project
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# Create the WSGI application object
 application = get_wsgi_application()
