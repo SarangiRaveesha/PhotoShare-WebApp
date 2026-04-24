2fa6e9e7569c36fdf4d0dec70e1a5b7d8be53c11
rushani savishka <rushanisavishka@gmail.com>
2026-04-24 11:56:38 +0530

    Refactor user models and maintain functionality

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
-
+# Profile model extends with additional information
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
     bio = models.TextField(blank=True)
